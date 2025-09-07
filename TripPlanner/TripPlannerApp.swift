3e0d925242cf425dacec51ea03c86080e62a1bb9
shivakumarchirra <chirrashivakumar5@gmail.com>
2025-09-07 18:58:46 +0530

    Initial Commit

---
+++ TripPlanner/TripPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripPlannerApp.swift
+//  TripPlanner
+//
+//  Created by shivakumar chirra on 07/09/25.
+//
+
+import SwiftUI
+
+@main
+struct TripPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TripPlannerTests/TripPlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TripPlannerTests.swift
+//  TripPlannerTests
+//
+//  Created by shivakumar chirra on 07/09/25.
+//
+
+import Testing
+@testable import TripPlanner
+
+struct TripPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
